2d1416a29ef856e551b297dbf8f5f8013439b00d
Pranaya Shrestha <sthapranaya890@gmail.com>
2025-06-25 22:20:22 +0545

    student all done admin transaction and report generation left

--- project 2/lib/library/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Book, Member, Transaction, Fine
-
-admin.site.register(Book)
-admin.site.register(Member)
-admin.site.register(Transaction)
-admin.site.register(Fine)
